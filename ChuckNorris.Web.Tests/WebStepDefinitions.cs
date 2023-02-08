f33bbc83993a70d564278373d22768c217180051
chris.bimson <christopher.bimson@gmail.com>
2023-02-08 17:54:13 +0000

    Some simple specs for Chuck Norris facts via a browser.

---
+++ ChuckNorris.Web.Tests/WebStepDefinitions.cs
@@ -0,0 +1,67 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace ChuckNorris.Web.Tests
+{
+    [Binding]
+    public class WebStepDefinitions
+    {
+        private ScenarioContext context;
+
+        public WebStepDefinitions(ScenarioContext context)
+        {
+            this.context = context;
+        }
+
+        [Given(@"a Boomer")]
+        public void GivenABoomer()
+        {
+            IWebDriver driver = new ChromeDriver();
+            context.Set(driver);
+        }
+
+        [When(@"the Boomer asks for a random Chuck Norris fact")]
+        public void WhenTheBoomerAsksForARandomChuckNorrisFact()
+        {
+            var driver = context.Get<IWebDriver>();
+            driver.Navigate().GoToUrl("https://api.chucknorris.io/");
+            driver.FindElement(By.CssSelector("[href*='/jokes/random']")).Click();
+        }
+
+        [Then(@"the fact must contain the words ""([^""]*)""")]
+        public void ThenTheFactMustContainTheWords(string term)
+        {
+            var driver = context.Get<IWebDriver>();
+            try
+            {
+                Assert.Contains(term, driver.PageSource);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        [When(@"the Boomer asks for Chuck Norris facts about Jason Statham")]
+        public void WhenTheBoomerAsksForChuckNorrisFactsAboutJasonStatham()
+        {
+            var driver = context.Get<IWebDriver>();
+            driver.Navigate().GoToUrl("https://api.chucknorris.io/jokes/search?query=statham");
+        }
+
+        [Then(@"there should be no facts")]
+        public void ThenThereShouldBeNoFacts()
+        {
+            var driver = context.Get<IWebDriver>();
+            try
+            {
+                Assert.DoesNotContain("statham", driver.PageSource);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ Specifications/ChuckNorris.feature
@@ -0,0 +1,16 @@
+﻿Feature: Chuck Norris Facts
+
+In order to be entertained by stale meme humor
+As a boomer
+I want to get random Chuck Norris facts
+
+
+Scenario: Random Chuck Norris Facts
+	Given a Boomer
+	When the Boomer asks for a random Chuck Norris fact
+	Then the fact must contain the words "Chuck Norris"
+
+Scenario: No-one cares about Jason Statham
+	Given a Boomer
+	When the Boomer asks for Chuck Norris facts about Jason Statham
+	Then there should be no facts
